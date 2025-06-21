e62b2be34465edee7d463953aa0a5dcca75fbc6b
Negasonic0 <135484294+Negasonic0@users.noreply.github.com>
2025-06-21 11:57:53 -0500

    Update app.py

--- app.py
+++ app.py
@@ -27,4 +27,5 @@ def descargar():
     return render_template('descargar.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))     
+    app.run(host='0.0.0.0', port=port)
